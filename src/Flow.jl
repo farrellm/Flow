4a81bc5f6867312589c9aa0b4336451d7a43d665
Matthew Farrell <farrellm@alum.mit.edu>
2014-07-08 22:00:48 -0400

    add assoc_in

--- src/Flow.jl
+++ src/Flow.jl
@@ -2,10 +2,47 @@ module Flow
 
 using HttpCommon, Morsel
 
-app = Morsel.app()
 
+app = Morsel.app()
+state = Dict()
+
+
+## utility
 load_static(path) = open(readall, Pkg.dir("Flow") * "/" * path)
 
+assoc(m, k) = m[k]
+
+function assoc_in(m, ks, v)
+    merge(m, assoc_in(m, ks, v, 1))
+end
+function assoc_in(m, ks, v, i)
+    if i < length(ks)
+        if haskey(m, ks[i])
+            merge(m, assoc_in(m[ks[i]], ks, v, i+1))
+        else
+            n = [ks[i] => assoc_in(Dict(), ks, v, i+1)]
+            merge(m, n)
+        end
+    else
+        merge(m, {ks[end] => v})
+    end
+end
+
+function assoc_in!(m, ks, v)
+    n = m
+    for k = ks[1:end-1]
+        println(n)
+        if !haskey(n, k)
+            n[k] = Dict()
+        end
+        n = n[k]
+    end
+    println(n)
+    n[ks[end]] = v
+end
+
+
+## rest
 get(app, "/static/html/<path::%.*>") do req, res
     load_static("html/" * routeparam(req, :path))
 end
@@ -33,6 +70,8 @@ get(app, "/eval") do req, res
     string(r)
 end
 
+
+## process
 start() = Morsel.start(app, 8000)
 
 end # module
